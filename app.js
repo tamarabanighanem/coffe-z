9fbaf66a7cc89348981981d4da70e9c391333eb1
tamara <tbanighanem@gmail.com>
2023-03-19 15:32:58 +0300

    Ex2 will done

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 let name = prompt("please enter your name");
 let gender = prompt("please enter your Gender");
+while (gender != "male" && gender != "female") {
+    gender = prompt("please enter your correct Gender");
+
+
+}
 if (gender == "male") {
     alert("Welcome MR" + " " + name);
 
 }
-else if (gender == "femal") {
+else if (gender == "female") {
     alert("Welcome MS" + " " + name);
 }
 else {
     alert("Welcome" + " " + name);
 
 }
-confirm("Do you want a hot drink?");
-confirm("Do you want a cold drink?");
+
+
+let typeOfDrink = " "
+if (confirm("Do you want a hot drink?") == true) {
+    typeOfDrink = "hot";
+}
+else {
+    typeOfDrink = "cold";
+}
 let drink = prompt("what's the name of your drink");
 alert("your drink is" + " " + drink);
 console.log(name + " " + drink);
+let arr = [name, gender, typeOfDrink, drink];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
